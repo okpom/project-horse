34d806f79d49c2726d2cd2789ad854b31f71f7a8
Mushroomyploom <lewis.w.kynan@gmail.com>
2025-11-16 17:42:53 -0800

    turn system

--- scripts/boss_fight_manager.gd
+++ scripts/boss_fight_manager.gd
@@ -1,17 +1,28 @@
 class_name BossFightManager
 extends Node
 
-const PLAYER_SCENE = preload("res://scenes/entities/player.tscn")
+const PLAYER1_SCENE = preload("res://scenes/entities/player1.tscn")
+const PLAYER2_SCENE = preload("res://scenes/entities/player2.tscn")
 const BOSS_SCENE = preload("res://scenes/entities/boss.tscn")
 const MAX_PLAYERS = 2
 
 var players: Array[Entity] = []
 var boss: Entity
 
+#vars for turn order
+var turn_order: Array[Entity] = []
+var current_turn: int = 0
+
+#health display debug
+@onready var player1health = $CanvasLayer/player1health
+@onready var player2health   = $CanvasLayer/player2health
+@onready var bosshealth   = $CanvasLayer/bosshealth
+
 
 func _ready() -> void:
 	spawn_players()
 	spawn_boss()
+	update_health_display()
 	start_boss_fight()
 
 
@@ -19,10 +30,13 @@ func spawn_players() -> void:
 	var player_positions = [Vector2(-450, -200), Vector2(-400, 150)]
 	
 	for i in range(MAX_PLAYERS):
-		var player_instance = PLAYER_SCENE.instantiate() as Entity
+		var player_instance
+		if i == 0:
+			player_instance = PLAYER1_SCENE.instantiate() as Entity
+		if i == 1:
+			player_instance = PLAYER2_SCENE.instantiate() as Entity
 		player_instance.global_position = player_positions[i]
 		player_instance.name = "Player_%d" % (i + 1)
-		print(player_instance.name)
 		
 		add_child(player_instance)
 		players.append(player_instance)
@@ -39,6 +53,80 @@ func spawn_boss() -> void:
 
 
 func start_boss_fight() -> void:
+	#make turn order
+	_initialize_order()
+	#start combat logic
+	await _next_turn()
+
+
+func _initialize_order() -> Array[Entity]:
+	turn_order = players.duplicate()
+	turn_order.append(boss)
+	# Sort descending speed
+	turn_order.sort_custom(_compare_speed)
+	 
+	#print turn order
+	print ("TURN ORDER")
+	for entity in turn_order:
+		print("name: ", entity.name, ", speed: ", entity.speed)
+	return turn_order
+
+#func to calculate turn order; can be modified to account for speed ties?
+func _compare_speed(a,b):
+	# Higher speed first
+	return a.speed > b.speed
+
+# combat logic, called every turn
+func _next_turn() -> void:
+	var entity = turn_order[current_turn]
+	print("\n-- TURN:", entity.name, "--")
 	
-	# Transitions to the actual boss fight
-	pass
+	if entity is Player:
+		await _player_turn(entity)
+	else:
+		await _boss_turn(entity)
+	
+	# Move to next turn if fight is ongoing
+	current_turn = (current_turn + 1) % turn_order.size()
+	#need to add logic for dead party as well
+	if !boss.is_dead:
+		await _next_turn()
+	else:
+		print("win")
+
+#basic player turn logic; press space to attack
+func _player_turn(player: Entity) -> void:
+	print(player.name, " is choosing an action (press space)")
+	# skill selection goes here instead of wait for space
+	await _wait_for_space()
+	print(player.name, " attacks")
+	#placeholder damage
+	boss.take_damage(10)
+	print (boss.name, " takes 10 damage")
+	update_health_display()
+
+#replace this with skill selection
+func _wait_for_space() -> void:
+	await get_tree().create_timer(0.05).timeout  # delay to avoid double input
+	while true:
+		await get_tree().process_frame
+		if Input.is_action_just_pressed("ui_accept"):
+			return
+
+#boss turn; attacks randomly based on number of coins
+#need to add coins having their own skills and targeting and stuff
+func _boss_turn(boss_ent: Entity) -> void:
+	await get_tree().create_timer(0.5).timeout
+	for i in range(boss_ent.coins):
+		var target = randi_range(0, 1)
+		print ("boss attacks ", players[target].name)
+		players[target].take_damage(5)
+		print (players[target].name, " takes 5 damage")
+		update_health_display()
+		await get_tree().create_timer(0.5).timeout
+
+#update debug health display
+func update_health_display():
+	player1health.text = "P1 HP: %d" % players[0].current_hp
+	player2health.text = "P2 HP: %d" % players[1].current_hp
+	bosshealth.text   = "Boss HP: %d" % boss.current_hp
